e5d70aafc786c687c95ebc76ee472a53f6355f59
engineerA314 <engineerA314@github.com>
2024-07-28 19:08:20 -0700

    fix: delete max_token argument

--- dsp/modules/lm.py
+++ dsp/modules/lm.py
@@ -8,7 +8,6 @@ class LM(ABC):
         self.kwargs = {
             "model": model,
             "temperature": 0.0,
-            "max_tokens": 150,
             "top_p": 1,
             "frequency_penalty": 0,
             "presence_penalty": 0,
